f45d528dd635ef918e830380dc6ee6c1a5308aeb
Marc Stammerjohann <marcstammerjohann@web.de>
2020-03-11 15:09:36 +0100

    feat(national-parks): find many national parks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('nationalParks')
+  getNationalParks() {
+    return this.prisma.nationalPark.findMany();
   }
 }
